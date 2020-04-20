4674ea52b61d03f5eefdd0ac19a94133d9a16dcb
MB <mbilal_ce@live.com>
2020-04-20 12:21:10 +0100

    Tasks 1 to 7 done

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=mbilalce/housing-pred
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --image=$dockerpath housing-price-app --port=80
+sleep 60
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods 
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/housing-price-app 8000:80
